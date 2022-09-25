1aaa556aae5eed9b8f8e4a7452454de528bda724
atdilakshan <dilakshan0501@gmail.com>
2022-09-25 21:44:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// components
+import Board from "./components/Board";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 class="title">Drag And Drop List</h1>
+        <h3 class="profile">
+          Created By
+          <a href="https://atdilakshan.github.io" target="_blank" rel="noreferrer"> AT Dilakshan</a>
+        </h3>
       </header>
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+          <h2>Board 1</h2>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card One</p>
+          </Card>
+        </Board>
+
+        <Board id="board-2" className="board">
+          <h2>Board 2</h2>
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
+
+        <Board id="board-3" className="board">
+          <h2>Board 3</h2>
+          <Card id="card-3" className="card" draggable="true">
+            <p>Card Three</p>
+          </Card>
+        </Board>
+
+        <Board id="board-4" className="board">
+          <h2>Board 4</h2>
+          <Card id="card-4" className="card" draggable="true">
+            <p>Card Four</p>
+          </Card>
+        </Board>
+      </main>
     </div>
   );
 }
